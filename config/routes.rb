f61dac2240fea7579a80c71a0a3717b8eb96718c
jailson <jahylson_2@hotmail.com>
2025-11-13 22:13:48 +0000

    First commit

---
+++ config/routes.rb
@@ -0,0 +1,40 @@
+Rails.application.routes.draw do
+  resource :session
+  resources :passwords, param: :token
+  resource :sign_up
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "products#index"
+  resources :products
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
+  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+
+  # Defines the root path route ("/")
+  # root "posts#index"
+
+  namespace :settings do
+    resource :email, only: [ :show, :update ]
+    resource :password, only: [ :show, :update ]
+    resource :profile, only: [ :show, :update ]
+    resource :user, only: [ :show, :destroy ]
+
+    root to: redirect("/settings/profile")
+  end 
+  
+  namespace :email do
+      resources :confirmations, param: :token, only: [ :show ]
+  end  
+  
+  # Admins Only
+  namespace :store do
+    resources :products
+    resources :users
+      
+    root to: redirect("/store/products")
+  end
+end
